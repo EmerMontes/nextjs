0fa914125b32e18d3cf64ac6b8b9d5a82889eace
EDMR <emermontes15@gmail.com>
2023-12-02 01:22:09 -0500

    modificado page inicial

---
+++ src/app/about/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return <>
+    <h1>Ahora este es el about</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet hic ex praesentium. Quibusdam, magni quo? Optio, sequi nostrum ducimus doloribus natus corporis esse veniam laudantium enim deleniti minus quae! Repellendus?</p>
+    </> 
+  }
