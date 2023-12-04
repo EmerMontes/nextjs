96bbb25f0f4179b951d7f24aae8a66cd527e27e7
EDMR <emermontes15@gmail.com>
2023-12-03 21:56:15 -0500

    intentando trabajar con typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
-export default function Page() {
+
+const fetchUsers = async ()=>{
+  const res = await fetch('https://reqres.in/api/users')
+   const data: {
+    id: number,
+    email: string,
+    first_name: string,
+    last_name: string,
+    avatar: string
+  }[]= await res.json()
+  return data
+}
+
+
+ export default async function Page() {
+  const users: string[] = await fetchUsers()
+
   return <div>
 
   <h1>Hello, Next.js!</h1>
+  <section>
+    {users.map((user) => {
+      <p>Name {user.first_name}</p>
+    })}
+  </section>
   </div> 
 }
